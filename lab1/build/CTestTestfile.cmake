4797671b0b554978da4217f069adb9569bff4ef0
Class_Account <ee106a-ab@m92p-9>
2015-09-02 13:00:52 -0700

    Initial commit Lab 1, through to checkpoint 1

---
+++ lab1/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/src
+# Build directory: /home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(foo)
+SUBDIRS(bar)
---
+++ lab1/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/lib:/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/usr/sww/lib"
+export PATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/lib/pkgconfig:/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/build"
+export PYTHONPATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa15/class/ee106a-ab/ros_workspace/lab1/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
