315947662efc1f59959ea10a1ae56c8b0ee9b05a
Douglas Hutchings <ee106a-ab@m92p-7>
2015-10-13 22:59:16 -0700

    section 1.3 works.

--- lab6/src/ar_tag_subs.py
+++ lab6/src/ar_tag_subs.py
@@ -20,8 +20,14 @@ def return_rbt(trans, rot):
         (4,) array - rotation vector in quaternions
     """
 
+
     #YOUR CODE HERE
-    rbt = np.zeros(4,4)
+    
+    (omega, theta) = eqf.quaternion_to_exp(rot)
+
+    rbt = eqf.create_rbt(omega,theta,trans)
+
+
     return rbt
 
 def compute_twist(rbt):
@@ -36,9 +42,20 @@ def compute_twist(rbt):
         w - (3,) array
     """
     #YOUR CODE HERE
-    v = np.array(3,1)
-    w = np.array(3,1)
-    print (v,w)
+    
+    rot_matrix = rbt[0:3,0:3]
+    trans = rbt[0:3,3]
+    
+    (omega,theta) = eqf.find_omega_theta(rot_matrix)
+    v = eqf.find_v(omega,theta,trans)
+
+    v = v.flatten()
+
+    #v = np.array(3,1)
+    #w = np.array(3,1)
+    #print(v)
+    #print(omega)
+    return (v,omega)
 
 if __name__=='__main__':
     rospy.init_node('ar_tags_subs')
@@ -50,19 +67,38 @@ if __name__=='__main__':
     ar_tags['ar1'] = 'ar_marker_' + sys.argv[2]
     ar_tags['arZ'] = 'ar_marker_' + sys.argv[3]
 
+    print(ar_tags)
+
+    print(ar_tags['ar0'])
+
     listener = tf.TransformListener()
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
+        #print(dir(listener))
+        #print(listener.allFramesAsDot())
+
+        #print(listener.frameExists(ar_tags['arZ']))        
+
+        #print("AR TAGS EXISTANCE IS AS FOLLOWS: ")
+        #print(listener.frameExists(ar_tags['ar0']))
+        #print(listener.frameExists(ar_tags['ar1']))
+        #print(listener.frameExists(ar_tags['arZ']))
+
         try:
-            (trans, rot) = listener.lookupTransform(ar_tags['arZ'], ar_tags['ar1'], rospy.Time(0))
+            (trans, rot) = listener.lookupTransform(ar_tags['ar1'], ar_tags['arZ'], rospy.Time(0))
             rbt = return_rbt(trans=trans, rot=rot)
-            print('gab between ' + ar_tags['arZ'] + ' and ' + ar_tags['ar1'])
+            #rbt = return_rbt(np.array(trans),np.array(rot))
+            print('gab between ' + ar_tags['ar1'] + ' and ' + ar_tags['arZ'])
             print rbt
             twist = compute_twist(rbt=rbt)
-            print('twist between ' + ar_tags['arZ'] + ' and ' + ar_tags['ar1'])
+            print('twist between ' + ar_tags['ar1'] + ' and ' + ar_tags['arZ'])
             print twist
         except:
+            #print("AR TAGS EXISTANCE IS AS FOLLOWS: ")
+            #print(listener.frameExists(ar_tags['ar0']))
+            #print(listener.frameExists(ar_tags['ar1']))
+            #print(listener.frameExists(ar_tags['arZ']))
             print ''
 
         try:
@@ -74,6 +110,10 @@ if __name__=='__main__':
             print('twist between ' + ar_tags['ar0'] + ' and ' + ar_tags['ar1'])
             print twist
         except:
+            #print("AR TAGS EXISTANCE IS AS FOLLOWS: ")
+            #print(listener.frameExists(ar_tags['ar0']))
+            #print(listener.frameExists(ar_tags['ar1']))
+            #print(listener.frameExists(ar_tags['arZ']))
             print ''
             
         try:
@@ -85,6 +125,14 @@ if __name__=='__main__':
             print('twist between ' + ar_tags['ar0'] + ' and ' + ar_tags['arZ'])
             print twist
         except:
+            #print("AR TAGS EXISTANCE IS AS FOLLOWS: ")
+            #print(listener.frameExists(ar_tags['ar0']))
+            #print(listener.frameExists(ar_tags['ar1']))
+            #print(listener.frameExists(ar_tags['arZ']))
             print ''
+
+
+
+        #print(listener.lookupTransform(ar_tags['arZ'], ar_tags['ar1'],rospy.Time(0)))
 
         rate.sleep()
