fc0079d29b6e3139028edfcac5e9aa7aa16b3274
Douglas Hutchings <11douglash@berkeley.edu>
2015-12-06 22:31:16 -0800

    Commit so we can go to a new branch.

--- project/src/grid_marker_script.sh
+++ project/src/grid_marker_script.sh
@@ -2,18 +2,22 @@
 overall_string=''
 
 
-grid_size='1.2'
-count_offset='100'
+grid_size='0.70'
+count_offset='0'
 
 
+#don't forget to change the number on line 17 to be the maximum number on line 10~ PLUS ONE!
+#YOu'll waste a lot of time if you don't do this!
 for x in 0 1 2 3 4 5 6 7
 
 do
-	for y in 0 1 2 3 4 5 6 7
+	for y in 1 2 3 4 5 6 7 8
 
 	do
 
-	count=$(($x*3 + $y + $count_offset))
+	count=$(($x*8 + $y + $count_offset))
+
+	echo $count
 
 	xpos=$(expr $grid_size*$x | bc)
 	ypos=$(expr $grid_size*$y | bc)
@@ -31,6 +35,6 @@ do
 done
 
 overall_string=$overall_string$'-1\n'
-echo -ne $overall_string
+#echo -ne $overall_string
 
-echo -ne "$overall_string" | rosrun ar_track_alvar createMarker -s 0.75 -p
+echo -ne "$overall_string" | rosrun ar_track_alvar createMarker -uin -s 0.619 -p
