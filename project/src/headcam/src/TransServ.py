74a2328451afe3c4a18ebdd7385877be7d1fb51b
Douglas Hutchings <11douglash@berkeley.edu>
2015-12-07 10:17:49 -0800

    general cleanup of TransServ.py
    more comments, less unnecessary imports, a real usage string now.

--- project/src/headcam/src/TransServ.py
+++ project/src/headcam/src/TransServ.py
@@ -3,7 +3,6 @@ import sys
 import rospy
 # import roslib; roslib.load_manifest('kinematics')
 
-from moveit_msgs.msg import OrientationConstraint, Constraints
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Transform
 import numpy as np
@@ -22,35 +21,24 @@ from headcam.srv import *
 
 usage_str = \
 """
-Inverse kinematics script
+A server for TF Frames.
 
-Requires baxter.sh and abxter_moveit_noexec.launch to be run beforehand.
-Interactive mode allows control of the end-effector position via 
-text input or arrow keys.
+It is the server for a service 'new_tf_frames', that publishes broadcasts certian TF transforms, untill it is told not to.
 
-Keyboard-interactive controls:
-[UP, DOWN]      move along y-axis   Up is y+, Down is y-
-[LEFT, RIGHT]   move along x-axis.  Right is x+, Left is x-
-[Z, X]          move along z-axis
-[G]             toggle gripper
+a tag_persistance.srv message:
 
-Flags:
---help, -h      print usage instructions
--k              run interactive keyboard interface
--t              run interactive text interface
--l      tf_topic    ar_marker_number        Get transforms to a set tag using the tf service.
+Child: Child (new) TF frame's name
+Parent: the parent of the child.  If this is None, child is remove_index
+Tranfrom:  The transform from the parent to the child
+
 """
 
-last_seen = time.time()
 
 import tf
-global stop
-global dlist
 
 names = []
 parents = []
 transforms = []
-stop = False
 
 def movement_server():
     print("Initializing node... ")
@@ -100,12 +88,7 @@ def init():
         r.sleep()
 
 if __name__ == '__main__':
-  init()
-  # global ar_marker
-  # if (len(sys.argv) is not 2) and (len(sys.argv) is not 4):
-  #   print len(sys.argv)
-  #   print "error"
-  #   print sys.argv
-  # else:
-  #   ar_marker = sys.argv[1]
-  #   init()
+    if(len(sys.argv) > 1):
+        print usage_str
+    else:
+        init()
